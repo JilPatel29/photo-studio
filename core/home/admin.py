94ca43c2393b8fcc85b4efc809fe49c53e710547
JilPatel29 <jil.patel0529@gmail.com>
2024-10-20 15:06:14 +0530

    C

--- core/home/admin.py
+++ core/home/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import ModelName
 
-# Register your models here.
+@admin.register(ModelName)
+class ModelNameAdmin(admin.ModelAdmin):
+    list_display = ('field1', 'field2')  # Specify fields to display in the list view
+    search_fields = ('field1',)  # Fields you can search in the admin panel
--- core/home/models.py
+++ core/home/models.py
@@ -1,16 +1,11 @@
-# from home.utils import *
+# home/models.py
+
 from django.db import models
 
+class ModelName(models.Model):
+    # Define your fields here
+    field1 = models.CharField(max_length=100)
+    field2 = models.DateTimeField(auto_now_add=True)
 
-# Create your models here.
-
-class student(models.Model):
-    name=models.CharField(max_length=100)
-    age=models.IntegerField()
-    email=models.EmailField()
-    address=models.TextField()
-    imasge=models.ImageField()
-    file=models.FileField()
-
-
-    
+    def __str__(self):
+        return self.field1

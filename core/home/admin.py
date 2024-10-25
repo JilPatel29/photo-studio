35d6865a4d820776ff6407c96fdb7ba24a48d82b
JilPatel29 <jil.patel0529@gmail.com>
2024-10-25 10:07:46 +0530

    c

--- core/home/admin.py
+++ core/home/admin.py
@@ -1,16 +1,9 @@
-# from django.contrib import admin
-# from .models import ModelName
-
-# @admin.register(ModelName)
-# class ModelNameAdmin(admin.ModelAdmin):
-#     list_display = ('field1', 'field2')  # Specify fields to display in the list view
-#     search_fields = ('field1',)  # Fields you can search in the admin panel
-#     list_filter = ('category',)  # Add filters in the sidebar
-
-# # Register the model along with the custom admin class
-# admin.site.register(Service, ServiceAdmin)
 from django.contrib import admin
 from .models import Service
 
-# Register your model here
-admin.site.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'category')
+    search_fields = ('name',)
+    list_filter = ('category',)
+
+admin.site.register(Service, ServiceAdmin)
